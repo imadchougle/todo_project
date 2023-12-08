65d91b5e5301ad30f3e2d20d97d8fe051341ce03
imadchougle <imadchougle2@gmail.com>
2023-12-08 19:27:53 +0530

    views for todo_app

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
