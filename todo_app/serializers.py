13e4094bcbb8bee9e8b491714ac5db07ba1b7340
imadchougle <imadchougle2@gmail.com>
2023-12-08 19:53:41 +0530

    Updated serializers

--- todo_app/serializers.py
+++ todo_app/serializers.py
@@ -1,3 +1,5 @@
+# todo_app/serializers.py
+
 from rest_framework import serializers
 from .models import Todo, Tag
 
@@ -14,3 +16,15 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags', [])
+        todo_instance = Todo.objects.create(**validated_data)
+
+        tags = []
+        for tag_data in tags_data:
+            tag, _ = Tag.objects.get_or_create(name=tag_data.get('name'))
+            tags.append(tag)
+
+        todo_instance.tags.set(tags)
+        return todo_instance
