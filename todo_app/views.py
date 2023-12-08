8e5f1824c4126b25c31e40628d359ccb067a1edd
imadchougle <imadchougle2@gmail.com>
2023-12-08 23:43:08 +0530

    tests

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,16 +1,20 @@
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Todo
 from .serializers import TodoSerializer
-
 
 class TodoListCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class TodoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def partial_update(self, request, *args, **kwargs):
         kwargs['partial'] = True
