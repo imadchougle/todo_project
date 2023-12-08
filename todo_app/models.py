3fedbe1a24506e07a1c281c6e5e71abba645460f
imadchougle <imadchougle2@gmail.com>
2023-12-08 19:22:52 +0530

    Created Models

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    STATUS_CHOICES = [
+        ('OPEN', 'Open'),
+        ('WORKING', 'Working'),
+        ('DONE', 'Done'),
+        ('OVERDUE', 'Overdue'),
+    ]
+
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    due_date = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='OPEN')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
