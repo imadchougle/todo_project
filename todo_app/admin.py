f9079719eaa2ee37550b0a6fd044fe82c7d801b6
imadchougle <imadchougle2@gmail.com>
2023-12-08 19:25:51 +0530

    Registered admin

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Todo, Tag
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'timestamp')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Todo, TodoAdmin)
+admin.site.register(Tag, TagAdmin)
