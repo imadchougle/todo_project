e41b55eb7adaccb8da6280a009a419756a42ec69
imadchougle <imadchougle2@gmail.com>
2023-12-08 22:24:43 +0530

    updated validations

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class Todo(models.Model):
     STATUS_CHOICES = [
--- todo_app/serializers.py
+++ todo_app/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Todo, Tag
+from django.utils import timezone
+
 
 
 class TagSerializer(serializers.ModelSerializer):
