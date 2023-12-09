370a2488a82f8270a958833ac0f592f052339763
imadchougle <imadchougle2@gmail.com>
2023-12-09 18:54:53 +0530

    improved linting

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Todo, Tag
 
+
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'timestamp')
     list_filter = ('status',)
@@ -9,6 +10,7 @@ class TodoAdmin(admin.ModelAdmin):
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
 
 admin.site.register(Todo, TodoAdmin)
 admin.site.register(Tag, TagAdmin)
