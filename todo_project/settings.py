409ff7462024f990e6b56debce766a03b699fbff
imadchougle <imadchougle2@gmail.com>
2023-12-09 19:07:28 +0530

    improved linting

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nqx4u2jg$7k4pv94_!*3!(_mu^pgm&!*(x737zfrerdt_hhh6$'
+SECRET_KEY = (
+    'django-insecure-nqx4u2jg$7k4pv94_!*3!(_mu^pgm&!*(x737zfrerdt_hhh6$'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
