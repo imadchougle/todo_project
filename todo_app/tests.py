8fc18cc54b5b1ca4047ff53138c5ae458f8bbee3
imadchougle <imadchougle2@gmail.com>
2023-12-09 19:23:43 +0530

    updated tests

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -36,4 +36,4 @@ class TodoListViewTest(TestCase):
     def test_todo_list_view(self):
         response = self.client.get(reverse('todo-list-create'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'todo_list.html')
+        self.assertEqual(response.accepted_media_type, 'application/json')
