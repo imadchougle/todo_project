3db6f7bffe6cb09434574be331f4af127abd160a
imadchougle <imadchougle2@gmail.com>
2023-12-09 19:16:25 +0530

    improved settings

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.auth import password_validation as auth_password_validation
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,23 +90,12 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME':
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME':
-            'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME':
-            'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME':
-            'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': f'{auth_password_validation}.UserAttributeSimilarityValidator'},
+    {'NAME': f'{auth_password_validation}.MinimumLengthValidator'},
+    {'NAME': f'{auth_password_validation}.CommonPasswordValidator'},
+    {'NAME': f'{auth_password_validation}.NumericPasswordValidator'},
 ]
 
 
