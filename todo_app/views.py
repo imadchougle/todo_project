96eba56058a295e691128642ed3c89c58fdd4d69
imadchougle <imadchougle2@gmail.com>
2023-12-09 10:45:11 +0530

    latest

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Todo
 from .serializers import TodoSerializer
 
+
 class TodoListCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
